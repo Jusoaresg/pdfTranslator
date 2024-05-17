fcd95011b4c1b61d2b661ad6f3ba2b1dce0fcf98
jusoaresg <julianosgreg@gmail.com>
2024-05-17 01:44:39 -0300

    feat: add multi-core in the code process
    Add multi-core and thread in the convert and translate phase
    Add progress bar in the translate phase

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
+import multiprocessing
+from pdf2docx.converter import cpu_count
 import translate
+<<<<<<< Updated upstream
 import os
+=======
+import io
+>>>>>>> Stashed changes
 from pdf2docx import Converter
 import fitz
-import time 
 import threading
+from tqdm import tqdm
+
+class Colors:
+    RED = "\033[91m"
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    WARNING = '\033[93m'
+    ENDTEXT = '\033[0m'
+
 
 threadsToUse = 6
 checkForErrors = input("Deseja verificar por erros ? (Deixe vazio caso queira): ")
@@ -25,6 +39,7 @@ def checkErrors(start, end):
     for i in range(int(start), int(end)):
         print(f'[PAGE INFO] Page: {i}')
 
+<<<<<<< Updated upstream
         cv = Converter(pdfFile)
         n = f'out{i}.docx'
 
@@ -34,6 +49,29 @@ def checkErrors(start, end):
         except Exception as ex:
             print("Error")
             errors.append(f'Error: {ex} in page {i}')
+=======
+    try:
+        for i in range(int(start), int(end)):
+            print(f'{Colors.GREEN} [PAGE INFO] Page: {i} {Colors.ENDTEXT}')
+
+            output_stream = io.BytesIO()
+
+            try:
+                cv.convert(output_stream, start=i, end=i+1)
+            except Exception as ex:
+                print(f'{Colors.RED} [Error]: {ex} in page {i} {Colors.ENDTEXT}')
+                errors.append(f'Error: {ex} in page {i}')
+            finally:
+                output_stream.close()
+
+                print(Colors.WARNING)
+                progressBar.update(1)
+
+    except Exception as ex:
+        print("Erro no for")
+    finally:
+        progressBar.close()
+>>>>>>> Stashed changes
         cv.close()
 
 
@@ -42,6 +80,9 @@ if(checkForErrors == ""):
     threads = []
     pages = countPages(pdfFile)
     pagesDivided = pages/threadsToUse
+
+    progressBar = tqdm(leave=False, total=pages, desc=f'{Colors.WARNING}  [Error check progress]', unit="pg")
+
     for i in range(threadsToUse):
         if (i == 0): 
             start = 0;
@@ -84,7 +125,8 @@ if(checkForErrors == ""):
 
 if len(errors) == 0:
     cv = Converter(pdfFile)
-    cv.convert("output.docx")
+    #cv.convert("output.docx")
+    cv.convert("output.docx", start=0, end=countPages(pdfFile), multi_processing=True, cpu_count=threadsToUse)
     cv.close()
 
-translate.translateDocx()
+translate.translateDocx(threadsToUse)
--- translate.py
+++ translate.py
@@ -1,5 +1,8 @@
 from deep_translator import GoogleTranslator
+import threading
 import docx
+from tqdm import tqdm
+
 
 def checkInteger(string):
     try:
@@ -8,19 +11,50 @@ def checkInteger(string):
     except ValueError:
         return False
 
-def translateDocx():
+
+def translateParagraph(doc, start, end, bar):
+    for i in range(start, end):
+        paragraph = doc.paragraphs[i].text
+        if(paragraph != "" and checkInteger(paragraph) == False):
+            translated = GoogleTranslator(source='en', target='pt').translate(doc.paragraphs[i].text)
+            doc.paragraphs[i].text = translated
+            bar.update(1)
+
+def translateDocx(threadsToUse):
     doc = docx.Document("output.docx")
 
     print(len(doc.paragraphs))
+    threads = []
 
+    paragraphs = len(doc.paragraphs)
+    dividedParagraphs = paragraphs//threadsToUse
 
+    paragraphsExcluded = 0
+    for k, p in enumerate(doc.paragraphs):
+        if p.text != "" and not checkInteger(p.text):
+            paragraphsExcluded = paragraphsExcluded + 1
 
-    for key, i in enumerate(doc.paragraphs):
+    progressBar = tqdm(leave=False, total=paragraphsExcluded, desc=f'[Paragraphs translated]', unit="pg")
 
-        if(i.text != "" and checkInteger(i.text) == False):
-            print(key)
-            #print(i.text)
-            translated = GoogleTranslator(source="en", target="pt").translate(i.text)
-            i.text = translated
+    for i in range(threadsToUse):
+        if i == 0:
+            start = 0
+            end = dividedParagraphs
+        else:
+            start = dividedParagraphs*i
+            end = start + dividedParagraphs + 1
+
+        if i == threadsToUse-1:
+            end = paragraphs
+
+        thread = threading.Thread(target=translateParagraph, args=(doc, start, end, progressBar))
+        threads.append(thread)
+        thread.start()
+
+    for key, thread in enumerate(threads):
+        thread.join()
+
+    progressBar.close()
+
 
     doc.save("outputTranslated.docx")
